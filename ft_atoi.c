07d1fe03b966462aefad32d8a988ee86d5a8c43a
ShitooLL <Shitoo.ll@gmail.com>
2021-01-11 22:08:36 +0100

    atoi fix

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,17 +6,19 @@
 /*   By: laliao <laliao@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/07 01:19:44 by laliao            #+#    #+#             */
-/*   Updated: 2021/01/11 02:32:15 by laliao           ###   ########.fr       */
+/*   Updated: 2021/01/11 21:45:09 by laliao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
+#include <limits.h>
 
 int	ft_atoi(const char *s1)
 {
-	int			i;
-	int			neg;
-	long int	nb;
+	int					i;
+	int					neg;
+	unsigned long int	nb;
 
 	nb = 0;
 	neg = 1;
@@ -26,7 +28,8 @@ int	ft_atoi(const char *s1)
 		i++;
 	if (s1[i] == '-' || s1[i] == '+')
 	{
-		neg = -neg;
+		if (s1[i] == '-')
+			neg = -neg;
 		i++;
 	}
 	while (s1[i] && s1[i] >= '0' && s1[i] <= '9')
@@ -34,5 +37,7 @@ int	ft_atoi(const char *s1)
 		nb = nb * 10 + s1[i] - '0';
 		i++;
 	}
+	if ((nb > LONG_MAX && neg > 0) || (nb - 1) > LONG_MAX)
+		nb = (neg > 0) ? 0 : 1;
 	return (nb * neg);
 }
